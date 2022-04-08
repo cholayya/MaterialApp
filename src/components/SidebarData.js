424e2dd4192cd7259402b52736e1d789edc77ece
Cholayya <cholayya9902@gmail.com>
2022-04-08 09:36:56 +0530

    first time initilization

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -5,37 +5,61 @@ import * as IoIcons from "react-icons/io";
 
 export const SidebarData =[
     {
-        title:'Home',
+        title:'Company Master',
         path:'/',
         icon:<AiIcons.AiFillHome/>,
         cName:'nav-text'
     },
     {
-        title:'Reports',
+        title:'Leave Master',
         path:'/reports',
         icon:<IoIcons.IoIosPaper/>,
         cName:'nav-text'
     },
     {
-        title:'Products', 
+        title:'Employee Master', 
         path:'/products',
         icon:<FaIcons.FaCartPlus/>,
         cName:'nav-text'
     },
     {
-        title:'Team',
+        title:'Leave Application Module',
         path:'/team',
         icon:<IoIcons.IoMdPeople/>,
         cName:'nav-text'
     },
     {
-        title:'Messages',
+        title:'Utilities',
         path:'/messages',
         icon:<FaIcons.FaEnvelopeOpenText/>,
         cName:'nav-text'
     },
     {
-        title:'Support',
+        title:'Change Passowrd',
+        path:'/support',
+        icon:<IoIcons.IoMdHelpCircle/>,
+        cName:'nav-text'
+    },
+    {
+        title:'Admin',
+        path:'/support',
+        icon:<IoIcons.IoMdHelpCircle/>,
+        cName:'nav-text'
+    },
+    {
+        title:'Reports',
+        path:'/support',
+        icon:<IoIcons.IoMdHelpCircle/>,
+        cName:'nav-text'
+    },
+    {
+        title:'View Reports',
+        path:'/support',
+        icon:<IoIcons.IoMdHelpCircle/>,
+        cName:'nav-text'
+    },
+    {
+        title:'LogOut',
         path:'/support',
         icon:<IoIcons.IoMdHelpCircle/>,
         cName:'nav-text'
